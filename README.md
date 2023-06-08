47035d25bb7299bdb80978fda0a9837ce1d8fb33
余俊瑜 <2933582448@qq.com>
2023-06-08 11:36:07 +0800

    1.0

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # 基于master分支添加了GUI界面
 
 # python运行
-`pip install -r requirements.txt `
-
-`python main.py`
+- 优点：可以进行二次开发，
+- 缺点：需依赖python环境、第三方库
+- 安装方法：
+  - `pip install -r requirements.txt `
+  - `python main.py`
 
 # exe运行
+- 优点：无需依赖任何环境，下载即可使用
+- 缺点：目前exe文件较大（待修复）
+- 下载地址：
+  - [下载压缩包，运行main.exe](https://github.com/CrabBoss-lab/Baidu-Image-Loader/releases)
---
+++ setup.py
@@ -0,0 +1,24 @@
+from cx_Freeze import setup, Executable
+
+options = {
+    'build_exe': {
+        'packages': ['PySide6', 'requests'],
+        'include_files': ['bug.ico']
+    }
+}
+
+# TARGET
+executables = [
+    Executable('main.py', base=None)
+]
+
+# SETUP CX FREEZE
+setup(
+    name="百度图片采集器",
+    version="1.0",
+    description="百度图片采集器-基于master分支添加了GUI界面",
+    author="yujunyu",
+    options=options,
+    executables=executables
+
+)
